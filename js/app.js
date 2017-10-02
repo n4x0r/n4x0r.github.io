3cc3359f5a324869ed6522b3d060df5ff1628e5c
n4x0r <n4x0r@amn3s1a.com>
2017-10-02 10:14:08 -0700

    minor fixes

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ particlesJS('particles-js',
         "random": true,
         "anim": {
           "enable": false,
-          "speed": 20,
+          "speed": 10,
           "size_min": 0.1,
           "sync": false
         }
@@ -69,7 +69,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 4,
         "direction": "none",
         "random": false,
         "straight": false,
